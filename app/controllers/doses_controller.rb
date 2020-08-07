72465855cfe7b7e8348d050be6d75cbdf1dbe0bb
maxime berkowicz <maximeberkowicz@gmail.com>
2020-08-07 15:31:16 +0200

    first iteration

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+end
---
+++ app/models/dose.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :ingredient_id, presence: true
+  validates :cocktail_id, presence: true
+
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+end
